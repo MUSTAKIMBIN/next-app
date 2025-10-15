d2ad8589205453518f709bda5fce146e37f3350c
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-10-15 06:10:28 +0600

    route setups done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Components/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+  },
+]);
